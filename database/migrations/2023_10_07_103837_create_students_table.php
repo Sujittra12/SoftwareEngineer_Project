d9cf744045deb1b5212d840c63a973fd82755b07
Sujittra12 <mtanoy968@gmail.com>
2023-10-07 18:06:31 +0700

    database

---
+++ database/migrations/2023_10_07_103837_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            // id User จากตาราง user
+            $table->foreignId('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            //เก็บรหัสนศ.
+            $table->char('std_id',11)->comment('รหัสนักศึกษา');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
